afa089fb6886bacc0dc64fa2bf04f68da4984e31
javierSolanoP <jmsolano24@misena.edu.co>
2021-10-21 17:11:49 -0500

    Se agrega nuevamente el backend

--- backend/app/Http/Controllers/shift_module/TimeController.php
+++ backend/app/Http/Controllers/shift_module/TimeController.php
@@ -12,13 +12,14 @@ class TimeController extends Controller
     // Metodo para retornar todos los horarios de la tabla de la DB: 
     public function index()
     {
+
         // Realizamos la consulta a la tabla de la DB:
         $model = Time::select('start_time', 'finish_time');
         
         // Validamos que exista el registro en la tabla de la DB:
         $validateTime = $model->get();
 
-        // Si existen, los retornamos: 
+        // Si existen, loss retornamos: 
         if(count($validateTime) != 0){
 
             // Retornamos la respuesta:
